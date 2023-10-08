4101fc67a48b8e78fb97b7e1d86a102309db029d
skcajs <sjackson864@outlook.com>
2023-10-08 19:33:24 +0200

    added depth

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@
 const VSHADER_SOURCE = `
 attribute vec4 a_Position;
 attribute vec4 a_Color;
+uniform mat4 u_MvpMatrix;
 varying vec4 v_Color;
 void main() {
-    gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    gl_Position = u_MvpMatrix * a_Position;
     v_Color = a_Color;
 }`;
 
@@ -34,28 +34,38 @@ function main() {
 
   const n = initVertexBuffers(gl);
 
-  // TODO: Define Matrices
+  // gl.enable(gl.DEPTH_TEST);
 
+  const modelMatrix = new Matrix4();
+  const viewMatrix = new Matrix4();
+  const projectionMatrix = new Matrix4();
+  const mvPMatrix = new Matrix4();
+
+  const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix')
+  if(!u_MvpMatrix) {
+    console.log("error Retrieving location of u_MvpMatrix");
+    return;
+  }
 
   // TODO: Pass in Matrices to corresponding functions
-  document.onkeydown = function(ev) { keydown(ev, gl, n) }
+  document.onkeydown = function(ev) { keydown(ev, gl, canvas, n, modelMatrix, viewMatrix, projectionMatrix, mvPMatrix, u_MvpMatrix) }
   
-  draw(gl, n)
+  draw(gl, canvas, n, modelMatrix, viewMatrix, projectionMatrix, mvPMatrix, u_MvpMatrix)
 }
 
 function initVertexBuffers(gl) {
   var verticesColor = new Float32Array([
-     0.0,  0.5, -4.0,  0.4,  1.0,  0.4,
-    -0.5, -0.5, -4.0,  0.4,  1.0,  0.4,
+     0.0,  0.5,  0.0,  0.4,  1.0,  0.4,
+    -0.5, -0.5, -0.0,  0.4,  1.0,  0.4,
+     0.5, -0.5, -0.0,  1.0,  0.4,  0.4,
+
+     0.0,  0.5, -2.0,  1.0,  0.4,  0.4,
+    -0.5, -0.5, -2.0,  1.0,  1.0,  0.4,
+     0.5, -0.5, -2.0,  1.0,  1.0,  0.4,
+
+     0.0,  0.5, -4.0,  0.4,  0.4,  1.0,
+    -0.5, -0.5, -4.0,  0.4,  0.4,  1.0,
      0.5, -0.5, -4.0,  1.0,  0.4,  0.4,
-
-     0.5,  0.5, -2.0,  1.0,  0.4,  0.4,
-    -0.5,  0.5, -2.0,  1.0,  1.0,  0.4,
-     0.0, -0.5, -2.0,  1.0,  1.0,  0.4,
-
-     0.0,  0.5,  0.0,  0.4,  0.4,  1.0,
-    -0.5, -0.5,  0.0,  0.4,  0.4,  1.0,
-     0.5, -0.5,  0.0,  1.0,  0.4,  0.4,
   ]);
   const n = 9;
 
@@ -86,32 +96,51 @@ function initVertexBuffers(gl) {
   gl.enableVertexAttribArray(a_Color);
 
   return n;
-
 }
 
-let g_eyeX = 0; angle = 0;
+let g_eyeX = 0; angle = 0; projection = "p";
 
-function keydown(ev, gl, n) {
+function keydown(ev, gl, canvas, n, modelMatrix, viewMatrix, projectionMatrix, mvPMatrix, u_MvpMatrix) {
   if (ev.keyCode == 39) { // Right
-    g_eyeX -= 0.1;
+    g_eyeX -= 0.01;
   } else if (ev.keyCode == 37) { // Left
     g_eyeX += 0.01;
   } else if (ev.keyCode == 81) { // q
     angle += 1;
   } else if (ev.keyCode == 69) { // e
     angle -= 1;
-  } else { return; }
-  draw(ev, gl, n)
+  } else if (ev.keyCode == 79) { // o
+    projection = 'o';
+  } else if (ev.keyCode == 80) { // p
+    projection = 'p';
+  } 
+  else { return; }
+  draw(gl, canvas, n, modelMatrix, viewMatrix, projectionMatrix, mvPMatrix, u_MvpMatrix)
 }
 
-function draw(gl, n) {
-
+function draw(gl, canvas, n, modelMatrix, viewMatrix, projectionMatrix, mvPMatrix, u_MvpMatrix) {
     // TODO: Update the angles and eye point
+    modelMatrix.setRotate(angle, 0, 0, 1);
+    
+    switch(projection) {
+      case 'o':
+        viewMatrix.setLookAt(g_eyeX, 0, 5, 0, 0, 0, 0, 1, 0);
+        projectionMatrix.setOrtho(-1.0,1.0,-1.0,1.0,0.0,16.0);
+        break;
+      case 'p':
+        viewMatrix.setLookAt(g_eyeX, 0, 3.725, 0, 0, -100, 0, 1, 0);
+        projectionMatrix.setPerspective(30, canvas.width / canvas.height, 1, 100)
+        break;
+    }
+
+    mvPMatrix.set(projectionMatrix).multiply(viewMatrix).multiply(modelMatrix)
+
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvPMatrix.elements)
 
     // Set the clear color
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the canvas
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.DEPTH_BUFFER | gl.COLOR_BUFFER_BIT);
   
     // Draw Point
     gl.drawArrays(gl.TRIANGLES, 0, n);
